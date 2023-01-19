3a4d6a48cc8b45a0b99c70153eb180a0364abb6c
ziggornif <matthieudrouian@gmail.com>
2023-01-19 20:18:58 +0100

    feat: ui api calls

--- apps/rhfront/src/App.vue
+++ apps/rhfront/src/App.vue
@@ -2,54 +2,56 @@
   <main class="container">
     <div class="grid">
       <div>
-    <form @submit.prevent="searchEmployee">
-      <div class="grid">
-        <input v-model="searchTerm" placeholder="Rechercher">
-        <button type="submit">Rechercher</button>
+        <h1>RhTest</h1>
+        <form>
+          <div class="grid">
+            <input v-model="searchTerm" placeholder="Rechercher">
+            <button @click="search(searchTerm)">Rechercher</button>
+          </div>
+        </form>
+        <form @submit.prevent="createOrUpdateEmployee">
+          <label>Matricule: <input v-model="employee.id" placeholder="ID"></label>
+          <div class="grid">
+            <label>Nom: <input v-model="employee.name" placeholder="Nom"></label>
+            <label>Prénom: <input v-model="employee.lastname" placeholder="Prénom"></label>
+          </div>
+          <div class="grid">
+            <label>Evolution: <input v-model="employee.level" placeholder="Niveau"></label>
+            <label>Salaire: <input v-model="employee.salary" placeholder="Salaire"></label>
+          </div>
+          <div class="grid">
+            <button @click="createEmployee(employee)">Créer</button>
+            <button @click="updateEmployee(employee)">Modifier</button>
+          </div>
+        </form>
       </div>
-    </form>
-    <form @submit.prevent="createOrUpdateEmployee">
-      <label>Matricule: <input v-model="employee.id" placeholder="ID"></label>
-      <div class="grid">
-        <label>Nom: <input v-model="employee.name" placeholder="Nom"></label>
-        <label>Prénom: <input v-model="employee.lastname" placeholder="Prénom"></label>
+      <div>
+        <h2>Liste des salariés :</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Matricule</th>
+              <th>Nom</th>
+              <th>Prénom</th>
+              <th>Salaire</th>
+              <th>Evolution</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="employee in employees" :key="employee.id">
+              <td>{{ employee.id }}</td>
+              <td>{{ employee.name }}</td>
+              <td>{{ employee.lastname }}</td>
+              <td>{{ employee.salary }}</td>
+              <td>{{ employee.level }}</td>
+              <td>
+                <button @click="deleteEmployee(employee)">X</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <div class="grid">
-        <label>Evolution: <input v-model="employee.level" placeholder="Niveau"></label>
-        <label>Salaire: <input v-model="employee.salary" placeholder="Salaire"></label>
-      </div>
-      <button type="submit">Créer/Modifier</button>
-    </form>
-    </div>
-
-    
-    
-
-      <table>
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Prénom</th>
-            <th>Salaire</th>
-            <th>Niveau</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr v-for="employee in employees" :key="employee.id">
-            <td>{{ employee.id }}</td>
-            <td>{{ employee.name }}</td>
-            <td>{{ employee.lastname }}</td>
-            <td>{{ employee.salary }}</td>
-            <td>{{ employee.level }}</td>
-            <td>
-              <button @click="editEmployee(employee)">Modifier</button>
-              <button @click="deleteEmployee(employee)">Supprimer</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
     </div>
   </main>
 </template>
@@ -77,14 +79,14 @@ export default {
   },
   methods: {
     async fetchEmployees() {
-      const { data } = await axios.get('/api/rechercher?mode=all');
+      const { data } = await axios.get('http://localhost:8080/api/rechercher?mode=all');
       this.employees = data;
     },
     async createOrUpdateEmployee() {
       if (this.employee.id) {
-        await axios.put(`/api/modifier?id=${this.employee.id}`, this.employee);
+        await axios.put(`http://localhost:8080/api/modifier?id=${this.employee.id}`, this.employee);
       } else {
-        await axios.post('/api/ajouter', this.employee);
+        await axios.post('http://localhost:8080/api/ajouter', this.employee);
       }
       this.employee = {
         id: '',
@@ -93,6 +95,39 @@ export default {
         salary: '',
         level: ''
       }
+    },
+    async createEmployee(employee) {
+      await axios.post(`http://localhost:8080/api/ajouter?id=${employee.id}&name=${employee.name}&lastname=${employee.lastname}&salary=${employee.salary}&level=${employee.level}`);
+      this.employee = {
+        id: '',
+        name: '',
+        lastname: '',
+        salary: '',
+        level: ''
+      }
+      return this.fetchEmployees();
+    },
+    async updateEmployee(employee) {
+      await axios.post(`http://localhost:8080/api/modifier?id=${employee.id}&name=${employee.name}&lastname=${employee.lastname}&salary=${employee.salary}&level=${employee.level}`);
+      this.employee = {
+        id: '',
+        name: '',
+        lastname: '',
+        salary: '',
+        level: ''
+      }
+      return this.fetchEmployees();
+    },
+    async deleteEmployee(employee) {
+      await axios.delete(`http://localhost:8080/api/supprimer?id=${employee.id}`);
+      this.employee = {
+        id: '',
+        name: '',
+        lastname: '',
+        salary: '',
+        level: ''
+      }
+      return this.fetchEmployees();
     }
   }
 }
